0e8bfa019d72e66c2d63f56f85bd666bba3ff3fe
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-09-14 16:22:14 +0100

    Update SharedFilePart.py

--- hive/app/domain/SharedFilePart.py
+++ hive/app/domain/SharedFilePart.py
@@ -1,12 +1,8 @@
-import hashlib
+from domain import CryptoUtils
 
 
 class SharedFilePart:
     def __init__(self, part_id, part_data):
         self.part_id = part_id
         self.part_data = part_data
-        self.sha256 = __get_sha_256(part_data)
-
-    @staticmethod
-    def __get_sha_256(part_data):
-        return hashlib.sha256(part_data).hexdigest()
+        self.sha256 = CryptoUtils.sha256(part_data)
