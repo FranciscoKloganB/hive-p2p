0ab326401f64871347618f27dc9c22de4a139b3e
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-09-15 18:45:38 +0100

    Update SharedFilePart.py

--- hive/app/domain/SharedFilePart.py
+++ hive/app/domain/SharedFilePart.py
@@ -6,6 +6,7 @@ from utils import ConvertUtils, CryptoUtils
 class SharedFilePart:
     """
     Represents a simulation over the P2P Network that tries to persist a file using stochastic swarm guidance
+
     :ivar markov_matrix: container object describing and implementing Markov Chain behaviour
     :type hive.domain.MarkovMatrix
     :ivar ddv: stochastic like list to define the desired distribution vector that this SharedFilePart is pursuing
@@ -19,7 +20,6 @@ class SharedFilePart:
     """
 
     def __init__(self, markov_matrix, ddv, part_id, part_data):
-
         """
         :param markov_matrix: container object describing and implementing Markov Chain behaviour
         :type hive.domain.MarkovMatrix
@@ -30,6 +30,7 @@ class SharedFilePart:
         :param part_data: Up to 2KB blocks of raw data that can be either strings or bytes
         :type bytes or str
         """
+        
         self.markov_matrix = markov_matrix
         self.desired_distribution = np.array(ddv).transpose()
         self.part_id = part_id
