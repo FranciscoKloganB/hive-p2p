5f1699af2eadacba7551835b46a27486dd568b45
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-09-15 18:45:07 +0100

    Update SharedFilePart.py
    Now each Shared File is considered an Agent, who carries a not only the content but who also knows the distribution he has to achieve and carries inside him a Markov Matrix to allow buckets to properly interact with them.

--- hive/app/domain/SharedFilePart.py
+++ hive/app/domain/SharedFilePart.py
@@ -1,20 +1,37 @@
+import numpy as np
+
 from utils import ConvertUtils, CryptoUtils
 
 
 class SharedFilePart:
     """
     Represents a simulation over the P2P Network that tries to persist a file using stochastic swarm guidance
-
-    :param :ivar part_id: the name of the shared file without extensions concatenated with a number for uniqueness
+    :ivar markov_matrix: container object describing and implementing Markov Chain behaviour
+    :type hive.domain.MarkovMatrix
+    :ivar ddv: stochastic like list to define the desired distribution vector that this SharedFilePart is pursuing
+    :type 1D line numpy.array
+    :ivar part_id: unique identifier for this file on the P2P network
     :type str
-    :param :ivar part_data: arbitrary string or binary data that constitutes the part content
+    :ivar part_data: base64 string corresponding to the actual contents of this file part
     :type str
-
     :ivar sha256: hash value resultant of applying sha256 hash function over part_data param
     :type str
     """
 
-    def __init__(self, part_id, part_data):
+    def __init__(self, markov_matrix, ddv, part_id, part_data):
+
+        """
+        :param markov_matrix: container object describing and implementing Markov Chain behaviour
+        :type hive.domain.MarkovMatrix
+        :param ddv: stochastic like list to define the desired distribution vector that this SharedFilePart is pursuing
+        :type list<float>
+        :param part_id: the name of the shared file without extensions concatenated with a number for uniqueness
+        :type str
+        :param part_data: Up to 2KB blocks of raw data that can be either strings or bytes
+        :type bytes or str
+        """
+        self.markov_matrix = markov_matrix
+        self.desired_distribution = np.array(ddv).transpose()
         self.part_id = part_id
         self.part_data = ConvertUtils.bytes_to_base64_string(part_data)
         self.sha256 = CryptoUtils.sha256(part_data)
