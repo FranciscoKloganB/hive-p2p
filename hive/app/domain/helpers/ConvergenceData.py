1598ce1cb4b4117b5328883b65eb43ad1d0804ee
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-10-09 20:03:53 +0100

    Create ConvergenceData to facilitate convergence tracking of files
    Avoids having three or more dictionaries for each file

---
+++ hive/app/domain/helpers/ConvergenceData.py
@@ -0,0 +1,33 @@
+class ConvergenceData:
+    """
+    Holds data that helps an domain.Hivemind keep track of converge in a simulation
+    :cvar __DEVIATION_TOLERANCE: percentage in which a value on a distribution vector can deviate from another in eq cmp
+    :type float
+    :ivar cswc: indicates how many consecutive steps a file has in convergence
+    :type int
+    :ivar max_swc: indicates the biggest set of consecutive steps throughout the simulaton for this file
+    :type int
+    :ivar swc: indicates at which steps a file has seen convergence
+    """
+
+    __DEVIATION_TOLERANCE = 0.01
+
+    def __init__(self):
+        self.cswc = 0
+        self.max_swc = 0
+        self.swc = []
+
+    @staticmethod
+    def __equal_distributions(one, another):
+        row_count = len(one)
+        if row_count != len(another):
+            return False
+        for i in range(0, row_count):
+            deviation = another[i] * ConvergenceData.__DEVIATION_TOLERANCE
+            lower_bound = another[i] - deviation
+            upper_bound = another[i] + deviation
+            if lower_bound < one[i] < upper_bound:
+                continue
+            else:
+                return False
+        return True
