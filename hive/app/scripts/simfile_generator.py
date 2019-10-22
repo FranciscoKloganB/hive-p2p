7b30c2c0409219c1e3f7a51de1bd5c25327e85cf
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-10-22 01:09:53 +0100

    Almost there with node uptime generation

--- hive/app/scripts/simfile_generator.py
+++ hive/app/scripts/simfile_generator.py
@@ -3,6 +3,9 @@ import sys
 import getopt
 import itertools
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from scipy.stats import skewnorm
 from globals.globals import SHARED_ROOT
 from scripts.continous_label_generator import yield_label
@@ -55,34 +58,55 @@ def __in_min_node_uptime():
             print("Input should be an float.. Try again;")
             continue
 
-def __generate_node_uptime():
-    print("Wait a few moments while uptimes are being generated...\nThis should take no longer than 30s...")
+
+def __generate_skewed_samples():
     max_uptime = 100.0
-    skewness = -80  # Negative values are left skewed, positive values are right skewed. DON'T REMOVE (-) sign
-    sample_distribution = skewnorm.rvs(a=skewness, size=1000000)  # Skewnorm function
-    sample_distribution = sample_distribution - min(sample_distribution)  # Shift the set so the minimum value is equal to zero
-    sample_distribution = sample_distribution / max(sample_distribution)  # Standadize all the vlues between 0 and 1.
-    sample_distribution = sample_distribution * max_uptime   # Multiply the standardized values by the maximum value.
-    return sample_distribution
-    # return min_uptime + compensation
+    skewness = -60.0  # Negative values are left skewed, positive values are right skewed. DON'T REMOVE (-) sign
+    samples = skewnorm.rvs(a=skewness, size=599999)  # Skewnorm function
+    samples = samples - min(samples)  # Shift the set so the minimum value is equal to zero
+    samples = samples / max(samples)  # Standadize all the vlues between 0 and 1.
+    samples = samples * max_uptime    # Multiply the standardized values by the maximum value.
+    return samples
+
+
+def __generate_skwed_samples_extended():
+    samples = __generate_skewed_samples()
+    n, bins, patches = plt.hist(samples, bins='auto', density=True)
+
+    bin_count = len(n)
+
+    total_density = 0.0
+    for i in range(bin_count):
+        total_density += n[i]
+
+    probabilities = []
+    total_probability = 0.0
+    for i in range(bin_count):
+        pvi = n[i] / total_density
+        total_probability += pvi
+        probabilities.append(pvi)
+
+    if total_probability != 1.0:
+        raise RuntimeError("Sample probabilities != 1.0 - Please report error at francisco.t.barros@tecnico.ulisboa.pt")
+
+
+def __print_node_uptime_distribution(samples):
+    n, bins, patches = plt.hist(samples, bins='auto', density=True)
+    plt.title("Peer Node Uptime Distribution")
+    plt.xlabel("uptime")
+    plt.ylabel("frequency")
+    plt.show()
 
 def __init_nodes_uptime_dict():
     nodes_uptime_dict = {}
     number_of_nodes = __in_number_of_nodes()
     min_uptime = __in_min_node_uptime()
-    sample_distribution = __generate_node_uptime()
-
+    print("Please wait... Generation of uptimes for each node may take a while.")
+    samples = __generate_skewed_samples()
+    node_uptime = np.random.choice(samples)
     for label in itertools.islice(yield_label(), number_of_nodes):
-        uptime = None
+        uptime = np.random.choice(a=samples)
         nodes_uptime_dict[label] = node_uptime
-
-def print_node_uptime_distribution(distribution):
-    import matplotlib.pyplot as pyplot
-    pyplot.hist(distribution, bins=10000, color='red', alpha=0.5)
-    pyplot.title("Peer Node Uptime Distribution")
-    pyplot.xlabel("uptime")
-    pyplot.ylabel("frequency")
-    pyplot.show()
 
 
 # noinspection DuplicatedCode
