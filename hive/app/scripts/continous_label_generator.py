8c099dc4bd5f1ba08e855d2641047d1c793a3359
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-10-22 11:39:49 +0100

    continous_label_generator.py adheres to PEP8 convention

--- hive/app/scripts/continous_label_generator.py
+++ hive/app/scripts/continous_label_generator.py
@@ -1,11 +1,13 @@
 import itertools
 from string import ascii_lowercase
 
+
 def yield_label():
     for size in itertools.count(1):
         for s in itertools.product(ascii_lowercase, repeat=size):
             print(s)
             yield "".join(s)
+
 
 def __generate_sequence(n):
     """
