52126a44366eeb92ee97d42e5f6db996eda0349d
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-10-23 23:57:26 +0100

    still trouble with packages

--- hive/app/simulation_file_generator.py
+++ hive/app/simulation_file_generator.py
@@ -8,8 +8,8 @@ import logging
 import itertools
 
 import numpy as np
-import hivescripts.label_generator as cg
-import hivescripts.skewed_distribution_generator as sg
+import generators.label_generator as cg
+import generators.skewed_distribution_generator as sg
 
 from pathlib import Path
 from globals.globals import SHARED_ROOT, DEBUG
