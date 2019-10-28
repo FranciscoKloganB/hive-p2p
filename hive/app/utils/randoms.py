7f39fc785a08a2ba569d00c1d893ac8795b14f16
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-10-28 23:44:53 +0000

    Fixed a bug in excluding_randrange

--- hive/app/utils/randoms.py
+++ hive/app/utils/randoms.py
@@ -1,16 +1,17 @@
 import random
 import numpy as np
-
+import logging
 
 def excluding_randrange(start, exclude_from, exclude_to, stop, step=1):
-    if start < exclude_from < exclude_to < stop:
-        raise RuntimeError("Ranges aren't properly defined. Convention is start < exclude_from < exclude_to < stop ")
+    if (exclude_from <= start) or (stop <= exclude_to) or (exclude_to <= exclude_from):
+        logging.error("{} < {} < {} < {}\n".format(start, exclude_from, exclude_to, stop))
+        raise ValueError("Ranges aren't properly defined. Convention is start < exclude_from < exclude_to < stop ")
 
     randint_1 = random.randrange(start, exclude_from, step)  # [start, exclude_from)
     randint_2 = random.randrange(exclude_to, stop, step)  # [exclude_to, stop)
 
-    total_range = stop - start
-    range_1 = exclude_from - start
-    range_2 = stop - exclude_to
+    range_1_size = exclude_from - start
+    range_2_size = stop - exclude_to
+    unified_size = range_1_size + range_2_size
 
-    return np.random.choice(a=[range_1, range_2], p=[range_1/total_range, range_2/total_range])
+    return np.random.choice(a=[randint_1, randint_2], p=[range_1_size/unified_size, range_2_size/unified_size]).item()
