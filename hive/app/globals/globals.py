e9b305abc17f9dab3ee5bd8984d4ca315eadb5b7
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-10-30 19:44:35 +0000

    Moved DEFAULT_COLUMN to globals

--- hive/app/globals/globals.py
+++ hive/app/globals/globals.py
@@ -1,6 +1,13 @@
 import os
 
 DEBUG = False
+
+# region integer constants
 READ_SIZE = 2048
+DEFAULT_COLUMN = 0
+# endregion
+
+# region path constants
 SHARED_ROOT = os.path.join(os.getcwd(), 'static', 'shared')
 SIMULATIONS_ROOT = os.path.join(os.getcwd(), 'static', 'simfiles')
+# endregion
