35f8f7e9beef17a5b3829b8b6bd7bb7ca1d4a9d3
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-10-30 19:06:58 +0000

    Update FileData.py

--- hive/app/domain/helpers/FileData.py
+++ hive/app/domain/helpers/FileData.py
@@ -1,3 +1,5 @@
+from domain.helpers.ConvergenceData import ConvergenceData
+
 class FileData:
     # region docstrings
     """
@@ -5,19 +7,35 @@ class FileData:
     to be within the long-term highest density node among other information.
     :ivar file_name: the name of the file
     :type str
-    :ivar part_count: how many parts exist for the
+    :ivar parts_count: how many parts exist for the
     :type int
     :ivar highest_density_node: label of the highest density node
     :type str
     :ivar highest_density_node_density: file density for the highest density node
     :type float
+    :ivar desired_distribution: desired distribution that sharers of this file must achieve together but independently
+    :type pandas.Dataframe, column vector with labels
+    :ivar current_distritubiton: keeps track of file current distribution, at each discrete time stage
+    :type dict<str, list<float>>
+    :ivar convergence_data: instance object with general information perteining the simulation
+    :type domain.helpers.ConvergenceData
     """
     # endregion
 
     # region class variables, instance variables and constructors
-    def __init__(self, file_name="", parts_count=0, node_name="", density=0.0):
+    def __init__(self,
+                 file_name="",
+                 parts_count=0,
+                 node_name="",
+                 density=0.0,
+                 ddv=None,
+                 cdv=None,
+                 convergence_data=None):
         self.file_name = file_name
         self.parts_count = parts_count
         self.highest_density_node = node_name
         self.highest_density_node_density = density
+        self.desired_distribution = ddv
+        self.current_distritubiton = cdv
+        self.convergence_data = convergence_data
     # endregion
