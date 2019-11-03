a27519844853f4ef9902f0faeda416f1296067f9
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-11-03 18:26:46 +0000

    Update FileData.py

--- hive/app/domain/helpers/FileData.py
+++ hive/app/domain/helpers/FileData.py
@@ -12,13 +12,13 @@ class FileData:
     :type str
     :ivar parts_count: how many parts exist for the
     :type int
-    :ivar highest_density_node: label of the highest density node
+    :ivar highest_density_node_label: label of the highest density node
     :type str
     :ivar highest_density_node_density: file density for the highest density node
     :type float
     :ivar desired_distribution: desired distribution that sharers of this file must achieve together but independently
     :type pandas.Dataframe, column vector with labels
-    :ivar current_distritubiton: keeps track of file current distribution, at each discrete time stage
+    :ivar current_distribution: keeps track of file current distribution, at each discrete time stage
     :type dict<str, list<float>>
     :ivar convergence_data: instance object with general information perteining the simulation
     :type domain.helpers.ConvergenceData
@@ -39,7 +39,7 @@ class FileData:
         self.highest_density_node_label = node_name
         self.highest_density_node_density = density
         self.desired_distribution = ddv
-        self.current_distritubiton = cdv
+        self.current_distribution = cdv
         self.convergence_data = convergence_data
     # endregion
 
@@ -50,10 +50,9 @@ class FileData:
         :type list<str>
         """
         ddv_len = len(self.desired_distribution)
-
         if not labels or len(labels) != ddv_len:
             raise ValueError("Can't reset FileData.current_distribution, incorrect labels length...")
-        self.current_distritubiton = pd.DataFrame([0] * ddv_len, index=labels)
+        self.current_distribution = pd.DataFrame([0] * ddv_len, index=labels)
         self.convergence_data.save_sets_and_reset()
         self.reset_density_data()
 
@@ -61,14 +60,17 @@ class FileData:
         self.highest_density_node_label = self.desired_distribution.idxmax().values[0]  # index/label of highval
         self.highest_density_node_density = self.desired_distribution.loc[self.highest_density_node_label][0]  # highval
 
+    def replace_node(self, replacement_dict):
+        self.desired_distribution.rename(index=replacement_dict, inplace=True)
+        self.current_distribution.rename(index=replacement_dict, inplace=True)
+        self.reset_density_data()
+
     def reset_convergence_data(self):
         self.convergence_data.save_sets_and_reset()
 
     def equal_distributions(self):
-        cD.equal_distributions(self.desired_distribution, self.current_distritubiton)
-    # endregion
+        cD.equal_distributions(self.desired_distribution, self.current_distribution)
 
-    # region instance class methods
     def get_failure_threshold(self):
         """
         :returns: the failure threshold for the given file
