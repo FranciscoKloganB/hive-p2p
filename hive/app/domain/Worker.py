2e56f0011d67b19a500ef98dd14ab28bf0a8ead2
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-11-07 15:58:14 +0000

    Clearer param name

--- hive/app/domain/Worker.py
+++ hive/app/domain/Worker.py
@@ -108,10 +108,10 @@ class Worker:
         print("part_name: {}, part_number: {} - corrupted".format(part.part_name, str(part.part_number)))
         self.init_recovery_protocol(part)
 
-    def receive_parts(self, sf_id_parts, sf_name=None, no_check=False):
+    def receive_parts(self, sf_id_parts_dict, sf_name=None, no_check=False):
         """
         Keeps incomming shared file parts along with the ones already owned by the Worker instance
-        :param sf_id_parts: mapping of shared file part id to SharedFileParts instances
+        :param sf_id_parts_dict: mapping of shared file part id to SharedFileParts instances
         :type dict<int, domain.SharedFilePart>
         :param sf_name: name of the file the parts belong to. sf_name must be set if method is called with no_check=True
         :type str
@@ -120,9 +120,9 @@ class Worker:
         """
         if no_check and sf_name is not None:
             # Use sf_name in param to leverage the pythonic way of merging dictionaries
-            self.sf_parts[sf_name].update(sf_id_parts)
+            self.sf_parts[sf_name].update(sf_id_parts_dict)
         else:
-            for sf_part in sf_id_parts.values():
+            for sf_part in sf_id_parts_dict.values():
                 # When adding one 1-by-1, no other param other than the SharedFilePart is needed... See receive_part
                 self.receive_part(sf_part, no_check=False)
 
