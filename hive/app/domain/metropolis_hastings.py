0a54d68e00231eb07d076af36f33136cec94aa62
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-11-11 21:26:17 +0000

    metropolis_hastings.py is now type hinted

--- hive/app/domain/metropolis_hastings.py
+++ hive/app/domain/metropolis_hastings.py
@@ -1,26 +1,24 @@
 import numpy as np
 
+from typing import List, Tuple
 from domain.exceptions.DistributionShapeError import DistributionShapeError
 from domain.exceptions.MatrixNotSquareError import MatrixNotSquareError
 
 
 # region module public functions
-def metropolis_algorithm(a, ddv, column_major_in=False, column_major_out=True):
+def metropolis_algorithm(
+        a: List[List[int]], ddv: List[float], column_major_in: bool = False, column_major_out: bool = True) -> np.ndarray:
     """
-    :param a: any adjacency matrix (list of lits)
-    :type list<list<float>>
-    :param ddv: a stochastic desired distribution vector
-    :type list<float>
-    :param column_major_in: indicates wether adj_matrix given in input is in row or column major form
-    :type bool
-    :param column_major_out: indicates wether to return transition_matrix output is in row or column major form
-    :type bool
-    :return: transition matrix that converges to ddv in the long term
-    :type: N-D numpy.array
+    Constructs a transition matrix with desired distribution as steady state
+    :param List[List[int]] a: any adjacency matrix (list of lits)
+    :param List[float]: a stochastic desired distribution vector
+    :param bool column_major_in: indicates wether adj_matrix given in input is in row or column major form
+    :param bool column_major_out: indicates wether to return transition_matrix output is in row or column major form
+    :returns np.ndarray transition_matrix: unlabeled transition matrix
     """
 
-    ddv = np.asarray(ddv)
-    a = np.asarray(a)
+    ddv: np.array = np.asarray(ddv)
+    a: np.ndarray = np.asarray(a)
 
     # Input checking
     if ddv.shape[0] != a.shape[1]:
@@ -31,13 +29,13 @@ def metropolis_algorithm(a, ddv, column_major_in=False, column_major_out=True):
     if column_major_in:
         a = a.transpose()
 
-    shape = a.shape
-    size = a.shape[0]
+    shape: Tuple[int, int] = a.shape
+    size: int = a.shape[0]
 
-    rw = _construct_random_walk_matrix(a, shape, size)
-    r = _construct_rejection_matrix(ddv, rw, shape, size)
+    rw: np.ndarray = _construct_random_walk_matrix(a, shape, size)
+    r: np.ndarray = _construct_rejection_matrix(ddv, rw, shape, size)
 
-    transition_matrix = np.zeros(shape=shape)
+    transition_matrix: np.ndarray = np.zeros(shape=shape)
 
     for i in range(size):
         for j in range(size):
@@ -51,37 +49,30 @@ def metropolis_algorithm(a, ddv, column_major_in=False, column_major_out=True):
 
 
 # region module private functions
-def _construct_random_walk_matrix(adj_matrix, shape, size):
+def _construct_random_walk_matrix(adj_matrix: np.ndarray, shape: Tuple[int, int], size: int) -> np.ndarray:
     """
-    :param adj_matrix: any adjacency matrix
-    :type N-D numpy.array
-    :param shape: size of adj_matrix #(lines, columns)
-    :type tuple(int, int)
-    :param size: #lines or #columns, for effeciency
-    :type int
-    :return rw: a random_walk over the adjacency matrix with uniform distribution
-    :type N-D numpy.array
+    Constructs a random walk over the adjacency matrix
+    :param np.ndarray adj_matrix: any adjacency matrix
+    :param Tuple[int, int] shape: size of adj_matrix #(lines, columns)
+    :param int size: the number of lines/columns matrix has. These should match the tuple values.
+    :returns np.ndarray rw: a random_walk over the adjacency matrix with uniform distribution
     """
-    rw = np.zeros(shape=shape)
+    rw: np.ndarray = np.zeros(shape=shape)
     for i in range(size):
-        ext_degree = np.sum(adj_matrix[i, :])  # states reachable from node i, counting itself (hence ext) [0, 1, 1] = 2
+        degree: numpy.int32 = np.sum(adj_matrix[i, :])  # all possible states reachable from state i, including self
         for j in range(size):
-            rw[i, j] = adj_matrix[i, j] / ext_degree
+            rw[i, j] = adj_matrix[i, j] / degree
     return rw
 
 
-def _construct_rejection_matrix(ddv, rw, shape, size):
+def _construct_rejection_matrix(ddv: np.array, rw: np.ndarray, shape: Tuple[int, int], size: int) -> np.ndarray:
     """
-    :param ddv: a stochastic desired distribution vector
-    :type N-D numpy.array
-    :param rw: a random_walk over an adjacency matrix
-    :type N-D numpy.array
-    :param shape: size of adj_matrix #(lines, columns)
-    :type tuple(int, int)
-    :param size: #lines or #columns, for effeciency
-    :type int
-    :return r: a matrix containing acceptance/rejectance probabilities for the random walk
-    :type N-D numpy.array
+    Constructs a rejection matrix for the random walk
+    :param np.ndarray ddv: a stochastic desired distribution vector
+    :param np.ndarray rw: a random_walk over an adjacency matrix
+    :param Tuple[int, int] shape: size of adj_matrix #(lines, columns)
+    :param int size: #lines or #columns, for effeciency
+    :returns np.ndarray r: a matrix containing acceptance/rejectance probabilities for the random walk
     """
     r = np.zeros(shape=shape)
     with np.errstate(divide='ignore', invalid='ignore'):
@@ -91,24 +82,17 @@ def _construct_rejection_matrix(ddv, rw, shape, size):
     return r
 
 
-def _mh_summation(rw, r, i):
+def _mh_summation(rw: np.ndarray, r: np.ndarray, i: int) -> np.int32:
     """
     Performs summation of the m-h branch when indices of m[i, j] are the same, i.e.: when i=j
-    :param rw: a random_walk over an adjacency matrix
-    :type N-D numpy.array
-    :param r: a matrix containing acceptance/rejectance probabilities for the random walk
-    :type N-D numpy.array
-    :param i: a fixed row index to simulate a simulation function
-    :type: int
-    :return: pii, the jump probability from state i to state i in the metropolis hastings output (transition matrix)
-    :type float
+    :param np.ndarray rw: a random_walk over an adjacency matrix
+    :param np.ndarray r: a matrix containing acceptance/rejectance probabilities for the random walk
+    :param int i: a fixed row index to simulate a simulation function
+    :returns float: pii, the probability of going from state i to j, where i = j
     """
-    size = rw.shape[0]
-    pii = rw[i, i]
+    size: int = rw.shape[0]
+    pii: np.int32 = rw[i, i]
     for k in range(size):
         pii += rw[i, k] * (1 - min(1, r[i, k]))
     return pii
 # endregion
-
-
-
