808af004ac5d7bdcf51123719f57265b2606661a
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-11-11 21:05:38 +0000

    SharedFIlePart type hinted and simplified

--- hive/app/domain/SharedFilePart.py
+++ hive/app/domain/SharedFilePart.py
@@ -2,59 +2,28 @@ from utils import convertions, crypto
 
 
 class SharedFilePart:
-    # region docstrings
     """
     Represents a simulation over the P2P Network that tries to persist a file using stochastic swarm guidance
-    :ivar part_name: original name of the file this part belongs to
-    :type str
-    :ivar part_number: unique identifier for this file on the P2P network
-    :type int
-    :ivar part_id: concatenation of part_name | part_number
-    :type str
-    :ivar part_data: base64 string corresponding to the actual contents of this file part
-    :type str
-    :ivar sha256: hash value resultant of applying sha256 hash function over part_data param
-    :type str
+    :ivar str part_name: original name of the file this part belongs to
+    :ivar int part_number: unique identifier for this file on the P2P network
+    :ivar str part_id: concatenation of part_name | part_number
+    :ivar str part_data: base64 string corresponding to the actual contents of this file part
+    :ivar str sha256: hash value resultant of applying sha256 hash function over part_data param
     """
-    # endregion
 
     # region class variables, instance variables and constructors
-    def __init__(self, part_name, part_number, part_data):
+    def __init__(self, part_name: str, part_number: int, part_data: bytes):
         """
-        :param part_name: original name of the file this part belongs to
-        :type str
-        :param part_number: number that uniquely identifies this file part
-        :type int
-        :param part_data: Up to 2KB blocks of raw data that can be either strings or bytes
-        :type bytes or str
+        Instantiates a SharedFilePart object
+        :param str part_name: original name of the file this part belongs to
+        :param int part_number: number that uniquely identifies this file part
+        :param bytes part_data: Up to 2KB blocks of raw data that can be either strings or bytes
         """
-        self.__part_name = part_name
-        self.__part_number = part_number
-        self.__part_id = part_name + "_#_" + str(part_number)
-        self.__part_data = convertions.bytes_to_base64_string(part_data)
-        self.__sha256 = crypto.sha256(self.__part_data)
-    # endregion
-
-    # region properties
-    @property
-    def part_name(self):
-        return self.__part_name
-
-    @property
-    def part_number(self):
-        return self.__part_number
-
-    @property
-    def part_id(self):
-        return self.__part_id
-
-    @property
-    def part_data(self):
-        return self.__part_data
-
-    @property
-    def sha256(self):
-        return self.__sha256
+        self.part_name: str = part_name
+        self.part_number: int = part_number
+        self.part_id: str = part_name + "_#_" + str(part_number)
+        self.part_data: str = convertions.bytes_to_base64_string(part_data)
+        self.sha256: str = crypto.sha256(self.part_data)
     # endregion
 
     # region override
