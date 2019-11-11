26ec831de89a6f0c0c67d35244ea4f16b0d0a4b0
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-11-11 18:29:14 +0000

    Type Hints in Enums

--- hive/app/domain/Enums.py
+++ hive/app/domain/Enums.py
@@ -2,13 +2,13 @@ from enum import Enum
 
 
 class Status(Enum):
-    SUSPECT = 1
-    OFFLINE = 2
-    ONLINE = 3
+    SUSPECT: int = 1
+    OFFLINE: int = 2
+    ONLINE: int = 3
 
 
 class HttpCodes(Enum):
-    OK = 200
-    NOT_FOUND = 404
-    TIME_OUT = 408
-    SERVER_DOWN = 521
+    OK: int = 200
+    NOT_FOUND: int = 404
+    TIME_OUT: int = 408
+    SERVER_DOWN: int = 521
