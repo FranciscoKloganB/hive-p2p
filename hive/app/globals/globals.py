81ac9ab0597e14ecb0dbb3278321f5461084515a
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2019-11-18 02:43:37 +0000

    Update globals.py

--- hive/app/globals/globals.py
+++ hive/app/globals/globals.py
@@ -8,6 +8,11 @@ MIN_CONVERGENCE_THRESHOLD: int = 5
 DEFAULT_COLUMN: int = 0
 # endregion
 
+# region float constants
+A_TOL: float = 1e-2
+R_TOL: float = 0.4
+# endregion
+
 # region path constants
 SHARED_ROOT: str = os.path.join(os.getcwd(), 'static', 'shared')
 OUTFILE_ROOT: str = os.path.join(os.getcwd(), 'static', 'outfiles')
