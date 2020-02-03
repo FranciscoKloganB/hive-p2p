45fceab585299f30e6030766fc38ce38fcc3dba1
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-02-03 11:54:59 +0000

    Updated simulation_file_generator, closes #19
    *Added __init_stochastic_vector_with_uptimes
    
    * Modified some variables names for readability, but not all.
    
    * Added type hints

--- hive/app/simulation_file_generator.py
+++ hive/app/simulation_file_generator.py
@@ -1,19 +1,21 @@
-import os
-import sys
 import copy
-import json
 import getopt
-import logging
-import random
 import itertools
+import json
+import logging
+import os
+import random
+import sys
+
+from decimal import Decimal
+from pathlib import Path
+from typing import List, Dict, Any
 
 import numpy as np
-from scripts.pyscripts import skewed_distribution_generator as sg, label_generator as cg
 
-from pathlib import Path
-from decimal import Decimal
 from domain.Hivemind import Hivemind
 from globals.globals import SHARED_ROOT, SIMULATION_ROOT
+from scripts.pyscripts import skewed_distribution_generator as sg, label_generator as cg
 
 
 # region usage
@@ -29,10 +31,9 @@ def usage():
 
 
 # region input consumption and checking functions
-def __in_max_stages():
+def __in_max_stages() -> int:
     """
-    :return max_stages: the number of stages this simulation should run at most
-    :type int
+    :return int max_stages: the number of stages this simulation should run at most
     """
     max_stages = input("\nEnter the maximum amount of stages [100, inf) the simulation should run: ")
     while True:
@@ -47,16 +48,12 @@ def __in_max_stages():
             continue
 
 
-def __in_number_of_nodes(msg, lower_bound=1, upper_bound=10000):
+def __in_number_of_nodes(msg: str, lower_bound: int = 1, upper_bound: int = 10000) -> int:
     """
-    :param msg: message to be printed to the user upon first input request
-    :type str
-    :param lower_bound: rejects any input equal or below it
-    :type int
-    :param upper_bound: rejects any input that is equal or above it
-    :type int
-    :return node_count: the number of nodes
-    :type int
+    :param str msg: message to be printed to the user upon first input request
+    :param int lower_bound: rejects any input equal or below it
+    :param int upper_bound: rejects any input that is equal or above it
+    :return int node_count: the number of nodes
     """
     node_count = input(msg)
     while True:
@@ -70,12 +67,10 @@ def __in_number_of_nodes(msg, lower_bound=1, upper_bound=10000):
             continue
 
 
-def __in_min_node_uptime(msg):
+def __in_min_node_uptime(msg: str) -> float:
     """
-    :param msg: message to be printed to the user upon first input request
-    :type str
-    :return min_uptime: minimum node uptime value
-    :type float
+    :param str msg: message to be printed to the user upon first input request
+    :return float min_uptime: minimum node uptime value
     """
     min_uptime = input(msg)
     while True:
@@ -89,10 +84,9 @@ def __in_min_node_uptime(msg):
             continue
 
 
-def __in_samples_skewness():
+def __in_samples_skewness() -> float:
     """
-    :return skewness: the skew value
-    :type float
+    :return float skewness: the skew value
     """
     print("\nSkewness should be [-100.0, 100.0]; Negative skews shift distribution mean to bigger positive values!")
     skewness = input("Enter the desired skewness for skewnorm distribution: ")
@@ -110,12 +104,10 @@ def __in_samples_skewness():
             continue
 
 
-def __in_file_name(msg):
+def __in_file_name(msg: str) -> str:
     """
-    :param msg: message to be printed to the user upon first input request
-    :type str
-    :return file_name: the name of the file to be shared
-    :type str
+    :param str msg: message to be printed to the user upon first input request
+    :return str file_name: the name of the file to be shared
     """
     file_name = input(msg).strip()
     while True:
@@ -128,12 +120,10 @@ def __in_file_name(msg):
         return file_name
 
 
-def __in_yes_no(msg):
+def __in_yes_no(msg: str) -> bool:
     """
-    :param msg: message to be printed to the user upon first input request
-    :type str
-    :return boolean: True or False
-    :type bool
+    :param str msg: message to be printed to the user upon first input request
+    :returns bool
     """
     char = input(msg + " y/n: ").lower()
     while True:
@@ -145,40 +135,34 @@ def __in_yes_no(msg):
             char = input("Answer should be 'y' for yes or 'n' for no... Try again: ")
 
 
-def __in_file_labels(node_uptime_dict, labels_list):
+def __in_file_labels(peer_uptime_dict: Dict[str, float], peer_names: List[str]) -> List[str]:
     """
-    :param node_uptime_dict: names of all peers in the system and their uptimes;
-    :type dict<str, float>
-    :param labels_list: names of all peers in the system; the length of labels must be >= desired_node_count
-    :type list<str>
-    :returns: a subset of :param labels_list; labels selected by the user which are known to exist
-    :type list<str>
+    :param Dict[str, float] peer_uptime_dict: names of all peers in the system and their uptimes;
+    :param List[str] peer_names: names of all peers in the system; the length of labels must be >= desired_node_count
+    :returns List[str]: a subset of :param labels_list; labels selected by the user which are known to exist
     """
     chosen_labels = input("The following labels are available:\n{}\nInsert a list of the ones you desire...\n"
                           "Example of a five label list input: a b c aa bcd\nTIP: You are not required to input"
                           "all labels manually...\nIf you assign less labels than required for the file "
                           "or accidently assign an unexisting label, the missing labels are automatically chosen"
-                          "for you!\n".format(node_uptime_dict)).strip().split(" ")
-    return [*filter(lambda label: label in labels_list, chosen_labels)]
+                          "for you!\n".format(peer_uptime_dict)).strip().split(" ")
+    return [*filter(lambda label: label in peer_names, chosen_labels)]
 
 
-def __in_adj_matrix(msg, size):
+def __in_adj_matrix(msg: str, size: int) -> List[List[int]]:
     """
     This method ensures the matrix is symmetric but doesn't to prevent transient state sets or absorbent nodes
-    :param msg: message to be printed to the user upon first input request
-    :type str
-    :param size: the size of the square matrix (size * size)
-    :type int
-    :return adj_matrix: the adjency matrix representing the connections between a group of peers
-    :type list<list<float>>
+    :param str msg: message to be printed to the user upon first input request
+    :param int size: the size of the square matrix (size * size)
+    :return List[List[int]] adj_matrix: the adjency matrix representing the connections between a group of peers
     """
     print(msg + "\nExample input for 3x3 matrix nodes would be:\n1 1 1\n1 1 0\n0 1 1\n")
     print("Warning: only symmetric matrices are accepted. Assymetric matrices may, but aren't guaranteed to converge!")
     print("Warning: this algorithm isn't well when adjency matrices have absorbent nodes or transient state sets!\n")
 
-    goto_while = False
+    goto_while: bool = False
     while True:
-        adj_matrix = []
+        adj_matrix: List[Any] = []
         for _ in range(size):
             row_vector = input().strip().split(" ")
             if len(row_vector) == size:
@@ -213,15 +197,12 @@ def __in_adj_matrix(msg, size):
         return adj_matrix
 
 
-def __in_stochastic_vector(msg, size):
+def __in_stochastic_vector(msg: str, size: int) -> List[float]:
     """
     This method guarantees the vector is stochastic
-    :param msg: message to be printed to the user upon first input request
-    :type str
-    :param size: the length of the vector
-    :type int
-    :return row_vector: the row_vector representing the desired distribution (steady state vector)
-    :type <list<float>
+    :param str msg: message to be printed to the user upon first input request
+    :param int size: the length of the vector
+    :return List[float] row_vector: the row_vector representing the desired distribution (steady state vector)
     """
     print(msg + "\nExample input stochatic vector for three nodes sharing a file would be: 0.35 0.15 0.5")
     while True:
@@ -238,72 +219,66 @@ def __in_stochastic_vector(msg, size):
 
 
 # region init and generation functions
-def __init_nodes_uptime_dict():
+def __init_nodes_uptime_dict() -> Dict[str, float]:
     """
-    :return nodes_uptime_dict: a dictionary the maps peers (state labels) to their machine uptimes.
-    :type dict<str, float>
+    :return Dict[str, float] peers_uptime_dict: a dictionary the maps peers (state labels) to their machine uptimes.
     """
     number_of_nodes = __in_number_of_nodes("\nEnter the number of nodes you wish to have in the network [2, 9999]: ")
     min_uptime = __in_min_node_uptime("\nEnter the mininum node uptime of nodes in the network [0.0, 100.0]: ")
+
     skewness = __in_samples_skewness()
-    print("\nPlease wait ¯\\_(ツ)_/¯ Generation of uptimes for each node may take a while.")
     samples = sg.generate_skewed_samples(skewness=skewness).tolist()
-    print("Keep calm. We are almost there...")
-    nodes_uptime_dict = {}
+
+    peers_uptime_dict = {}
     for label in itertools.islice(cg.yield_label(), number_of_nodes):
         uptime = abs(samples.pop())  # gets and removes last element in samples to assign it to label
         if uptime > 100.0:
-            nodes_uptime_dict[label] = 100.0
+            peers_uptime_dict[label] = 100.0
         elif uptime > min_uptime:
-            nodes_uptime_dict[label] = float(str(uptime)[:9])
+            peers_uptime_dict[label] = float(str(uptime)[:9])
         else:
-            nodes_uptime_dict[label] = min_uptime  # min_uptime was already truncated in __in_min_uptime
+            peers_uptime_dict[label] = min_uptime  # min_uptime was already truncated in __in_min_uptime
     samples.clear()
-    return nodes_uptime_dict
+    return peers_uptime_dict
 
 
-def __init_file_state_labels(desired_node_count, node_uptime_dict, labels_list):
+def __init_hive_members(desired_node_count: int, peers_uptime_dict: Dict[str, float], peer_names: List[str]) -> List[str]:
     """
-    :param desired_node_count: the number of peers that will be responsible for sharing a file
-    :type int
-    :param node_uptime_dict: names of all peers in the system and their uptimes;
-    :type dict<str, float>
-    :param labels_list: names of all peers in the system; the length of labels must be >= desired_node_count
-    :type list<str>
-    :return chosen_labels: a subset of :param labels; the peers from the system that were selected for sharing
-    :type list<str>
+    :param int desired_node_count: the number of peers that will be responsible for sharing a file
+    :param Dict[str, float] peers_uptime_dict: names of all peers in the system and their uptimes;
+    :param List[str] peer_names: names of all peers in the system; the length of labels must be >= desired_node_count
+    :return List[str] chosen_labels: a subset of :param labels; the peers from the system that were selected for sharing
     """
 
-    if len(labels_list) < desired_node_count:
+    if len(peer_names) < desired_node_count:
         raise RuntimeError("User requested that file is shared by more peers than the number of peers in the system")
 
     chosen_labels = []
-    labels_copy = copy.deepcopy(labels_list)
+    peer_names_copy = copy.deepcopy(peer_names)
 
     if __in_yes_no("\nDo you wish to manually insert some labels for this file?"):
-        chosen_labels = __in_file_labels(node_uptime_dict, labels_list)
-        labels_copy = [label for label in labels_copy if label not in chosen_labels]
+        chosen_labels = __in_file_labels(peers_uptime_dict, peer_names)
+        peer_names_copy = [label for label in peer_names_copy if label not in chosen_labels]
 
     chosen_count = len(chosen_labels)
     while chosen_count < desired_node_count:
         chosen_count += 1
-        choice = np.random.choice(a=labels_copy)
-        labels_copy.remove(choice)
+        choice = np.random.choice(a=peer_names_copy)
+        peer_names_copy.remove(choice)
         chosen_labels.append(choice)
     return chosen_labels
 
 
-def __init_adj_matrix(size):
+def __init_adj_matrix(size: int):
     """
     Generates a random symmetric matrix without transient state sets or absorbeent nodes
-    :param size: the size of the square matrix (size * size)
-    :type int
-    :return adj_matrix: the adjency matrix representing the connections between a group of peers
-    :type list<list<float>>
+    :param int size: the size of the square matrix (size * size)
+    :return List[List[int]] adj_matrix: the adjency matrix representing the connections between a group of peers
     """
     secure_random = random.SystemRandom()
-    adj_matrix = [[0] * size for _ in range(size)]
-    choices = [0, 1]
+    adj_matrix: List[List[int]] = [[0] * size for _ in range(size)]
+    choices: List[int] = [0, 1]
+
     for i in range(size):
         for j in range(i, size):
             probability = secure_random.uniform(0.0, 1.0)
@@ -312,7 +287,7 @@ def __init_adj_matrix(size):
 
     # Use guilty until proven innocent approach for both checks
     for i in range(size):
-        is_absorbent_or_transient = True
+        is_absorbent_or_transient: bool = True
         for j in range(size):
             # Ensure state i can reach and be reached by some other state j, where i != j
             if adj_matrix[i][j] == 1 and i != j:
@@ -324,13 +299,11 @@ def __init_adj_matrix(size):
     return adj_matrix
 
 
-def __init_stochastic_vector(size):
+def __init_stochastic_vector(size: int) -> List[float]:
     """
     Generates a row vector whose entries summation is one.
-    :param size: the length of the vector
-    :type int
-    :return stochastic_vector: the row_vector representing the desired distribution (steady state vector)
-    :type <list<float>
+    :param int size: the length of the vector
+    :return List[float] stochastic_vector: the row_vector representing the desired distribution (steady state vector)
     """
     secure_random = random.SystemRandom()
     stochastic_vector = [0.0] * size
@@ -348,29 +321,44 @@ def __init_stochastic_vector(size):
             summation_pool -= probability
 
 
-def __init_shared_dict(labels):
+def __init_stochastic_vector_with_uptimes(labels_list: List[str], uptime_dict: Dict[str, float]) -> List[float]:
+    """
+    Generates a row vector whose entries summation is one.
+    :param List[str] labels_list: contains the name of all peers, this param is required to ensure order is kept
+    :param Dict[str, float] uptime_dict: names of all peers in the system and their uptimes
+    :returns List[float]: desired distribution vector
+    """
+    stochastic_vector: List[float] = list()
+    for i in range(len(labels_list)):
+        stochastic_vector.append(uptime_dict[labels_list[i]])
+
+    uptimes_sum = sum(stochastic_vector)
+    return [uptime/uptimes_sum for uptime in stochastic_vector]
+
+
+def __init_shared_dict(peer_uptime_dict: Dict[str, float]) -> Dict[Dict[Any]]:
     """
     Creates the "shared" key of simulation file (json file)
-    :param labels: names of all peers in the system and their uptimes, the size of labels must be >= desired_node_count
-    :type dict<str, float>
-    :return shared_dict: the dictionary containing data respecting files to be shared in the system
-    :type dict<dict<obj>>
+    :param Dict[str, float] peer_uptime_dict: names of all peers in the system and their uptimes
+    :return Dict[Dict[Any]]shared_dict: the dictionary containing data respecting files to be shared in the system
     """
-    shared_dict = {}
-    labels_list = [*labels.keys()]
+    shared_dict: Dict[Any] = {}
+    peer_names: List[str] = [*peer_uptime_dict.keys()]
 
     print(
         "\nAny file you want to simulate persistance of must be inside the following folder: ~/hive/app/static/shared\n"
         "You may also want to keep a backup of such file in:  ~/hive/app/static/shared/shared_backups"
     )
 
-    add_file = True
+    add_file: bool = True
     while add_file:
         n = __in_number_of_nodes("Enter the number of nodes that should be sharing the next file: ")
         file_name = __in_file_name("Insert name of the file you wish to persist (include extension if it has one): ")
 
+        chosen_peers: List[str] = __init_hive_members(n, peer_uptime_dict, peer_names)
+
         shared_dict[file_name] = {}
-        shared_dict[file_name]["state_labels"] = __init_file_state_labels(n, labels, labels_list)
+        shared_dict[file_name]["state_labels"] = chosen_peers
 
         if __in_yes_no("Would you like to manually construct an adjency matrix?"):
             shared_dict[file_name]["adj_matrix"] = __in_adj_matrix("Insert a row major {}x{} matrix: ".format(n, n), n)
@@ -380,7 +368,7 @@ def __init_shared_dict(labels):
         if __in_yes_no("Would you like to manually insert a desired distribution vector?"):
             shared_dict[file_name]["ddv"] = __in_stochastic_vector("Insert a stochastic vector: ".format(n), n)
         else:
-            shared_dict[file_name]["ddv"] = __init_stochastic_vector(size=n)
+            shared_dict[file_name]["ddv"] = __init_stochastic_vector_with_uptimes(chosen_peers, peer_uptime_dict)
             pass
 
         add_file = __in_yes_no("Do you want to add more files to be shared under this simulation file?")
@@ -390,25 +378,19 @@ def __init_shared_dict(labels):
 
 
 # region actual main function
-def main(simfile_name):
+def main(simfile_name: str):
     """
     Creates a structured json file within the user's file system that can be used as input for an HIVE system simulation
-    :param simfile_name: name to be assigned to the simulation file (json file) in the user's file system
-    :type str
+    :param str simfile_name: name to be assigned to the simulation file (json file) in the user's file system
     """
-    if not simfile_name:
-        sys.exit("Invalid simulation file name - blank name not allowed)...")
-
-    file_path = os.path.join(SIMULATION_ROOT, simfile_name)
-
-    nodes_uptime_dict = __init_nodes_uptime_dict()
-    simfile_json = {
+    peers_uptime_dict: Dict[str, float] = __init_nodes_uptime_dict()
+    simfile_json: Dict[str, Any] = {
         "max_stages": __in_max_stages(),
-        "nodes_uptime": nodes_uptime_dict,
-        "shared": __init_shared_dict(nodes_uptime_dict)
+        "nodes_uptime": peers_uptime_dict,
+        "shared": __init_shared_dict(peers_uptime_dict)
     }
 
-    with open(file_path, 'w') as outfile:
+    with open(os.path.join(SIMULATION_ROOT, simfile_name), 'w') as outfile:
         json.dump(simfile_json, outfile)
 # endregion
 
@@ -416,19 +398,22 @@ def main(simfile_name):
 # region terminal comsumption function __main__
 # noinspection DuplicatedCode
 if __name__ == "__main__":
-    simfile_name_ = None
+    simfile_name_: str = ""
     try:
         options, args = getopt.getopt(sys.argv[1:], "ups:", ["usage", "plotuptimedistr", "simfile="])
         for options, args in options:
             if options in ("-u", "--usage"):
                 usage()
             if options in ("-p", "--plotuptimedistr"):
-                bin_count_ = int(input("How many bins should the distribution have?"))
-                sample_count_ = int(input("How many samples should be drawn?"))
+                bin_count_: int = int(input("How many bins should the distribution have?"))
+                sample_count_: int = int(input("How many samples should be drawn?"))
                 sg.plot_uptime_distribution(bin_count_, sample_count_)
             if options in ("-s", "--simfile"):
                 simfile_name_ = str(args).strip()
-                main(simfile_name_)
+                if simfile_name_:
+                    main(simfile_name_)
+                else:
+                    sys.exit("Invalid simulation file name - blank name not allowed)...")
     except getopt.GetoptError:
         usage()
 # endregion
