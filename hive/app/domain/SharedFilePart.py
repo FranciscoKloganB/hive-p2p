9508595b170c9b81d8e42be36c0a62167ab3c58e
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-02-04 16:28:39 +0000

    SharedFilePart now has attributed hive_id
    self.hive_id represents the hive entity responsible for persisting this file part

--- hive/app/domain/SharedFilePart.py
+++ hive/app/domain/SharedFilePart.py
@@ -4,31 +4,31 @@ from utils import convertions, crypto
 class SharedFilePart:
     """
     Represents a simulation over the P2P Network that tries to persist a file using stochastic swarm guidance
-    :ivar str part_name: original name of the file this part belongs to
-    :ivar int part_number: unique identifier for this file on the P2P network
-    :ivar str part_id: concatenation of part_name | part_number
-    :ivar str part_data: base64 string corresponding to the actual contents of this file part
+    :ivar str id: concatenation of part_name | part_number
+    :ivar str hive_id: uniquely identifies the hive that manages the shared file part instance
+    :ivar str name: original name of the file this part belongs to
+    :ivar int number: unique identifier for this file on the P2P network
+    :ivar str data: base64 string corresponding to the actual contents of this file part
     :ivar str sha256: hash value resultant of applying sha256 hash function over part_data param
     """
 
     # region class variables, instance variables and constructors
-    def __init__(self, part_name: str, part_number: int, part_data: bytes):
+    def __init__(self, hive_id: str, name: str, number: int, data: bytes):
         """
         Instantiates a SharedFilePart object
-        :param str part_name: original name of the file this part belongs to
-        :param int part_number: number that uniquely identifies this file part
-        :param bytes part_data: Up to 2KB blocks of raw data that can be either strings or bytes
+        :param str name: original name of the file this part belongs to
+        :param int number: number that uniquely identifies this file part
+        :param bytes data: Up to 2KB blocks of raw data that can be either strings or bytes
         """
-        self.part_name: str = part_name
-        self.part_number: int = part_number
-        self.part_id: str = part_name + "_#_" + str(part_number)
-        self.part_data: str = convertions.bytes_to_base64_string(part_data)
-        self.sha256: str = crypto.sha256(self.part_data)
+        self.id: str = name + "_#_" + str(number)
+        self.hive_id = hive_id
+        self.name: str = name
+        self.number: int = number
+        self.data: str = convertions.bytes_to_base64_string(data)
+        self.sha256: str = crypto.sha256(self.data)
     # endregion
 
     # region override
     def __str__(self):
-        return "part_name: {},\npart_number: {},\npart_id: {},\npart_data: {},\nsha256: {}\n".format(
-            self.part_name, self.part_number, self.part_id, self.part_data, self.sha256
-        )
+        return "part_name: {},\npart_number: {},\npart_id: {},\npart_data: {},\nsha256: {}\n".format(self.name, self.number, self.id, self.data, self.sha256)
     # endregion
