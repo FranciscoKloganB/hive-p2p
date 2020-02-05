269bed3bd176476437a9dcf9b855ff013d214672
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-02-05 21:27:22 +0000

    Added some HttpCodes

--- hive/app/domain/Enums.py
+++ hive/app/domain/Enums.py
@@ -16,6 +16,8 @@ class HttpCodes(Enum):
     """
     DUMMY: int = -1
     OK: int = 200
+    BAD_REQUEST: int = 400
     NOT_FOUND: int = 404
+    NOT_ACCEPTABLE: int = 406
     TIME_OUT: int = 408
     SERVER_DOWN: int = 521
