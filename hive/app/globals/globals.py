21d4f2458981f31b1de2e7ad5ddbbbab3302b176
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-02-05 22:21:49 +0000

    Update globals.py

--- hive/app/globals/globals.py
+++ hive/app/globals/globals.py
@@ -5,10 +5,12 @@ DEBUG: bool = True
 # region Simulation Settings
 READ_SIZE: int = 32768  # 32KB blocks. Should wield ~1440 SharedFileParts if the original file is ~45MB in size. With REPLICATION_LEVEL = 3, we run with 4320 parts
 MAX_EPOCHS = 720  # One day has 24h, meaning that one epoch per minute wwould be 1440, 720 defines one epoch every two minutes
-MIN_DETECTION_DELAY = 1  # 2 minutes
-MAX_DETECTION_DELAY = 7  # 14 minutes
+MIN_DETECTION_DELAY: int = 1  # 2 minutes
+MAX_DETECTION_DELAY: int = 7  # 14 minutes
+AVG_UPTIME: float = 0.4
 REPLICATION_LEVEL: int = 3  # Each file part has 3 copies, for simulation purposes, this copies are soft copies.
 MIN_CONVERGENCE_THRESHOLD: int = 3
+
 # endregion
 
 # region Integer Constants
@@ -16,7 +18,6 @@ DEFAULT_COLUMN: int = 0
 # endregion
 
 # region Float Constants
-AVG_UPTIME: float = 0.4
 A_TOL: float = 1e-2
 R_TOL: float = 0.4
 # endregion
