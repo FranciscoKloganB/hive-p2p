dc1b6594d932b91b3f081b5ebf67d99217f8e68c
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-06-09 19:50:49 +0100

    Update convex_optimization_mh.py

--- hive/app/utils/convex_optimization_mh.py
+++ hive/app/utils/convex_optimization_mh.py
@@ -47,6 +47,7 @@ def adjency_matrix_sdp_optimization(a: List[List[int]]) -> Tuple[float, np.ndarr
 def optimal_bilevel_mh_transition_matrix(A: np.ndarray, v_: np.ndarray) -> Tuple[np.ndarray, float]:
     """
     Constructs a transition matrix using linear programming relaxations and convex envelope approximations for the desired distribution vector.
+    Result is only trully optimal if cvx.norm(Topt - U, 2) is equal to the markov matrix eigenvalue.
     :param np.ndarray A: Symmetric unoptimized adjency matrix.
     :param np.ndarray v_: a stochastic desired distribution vector
     :returns Tuple[np.ndarray, float] (T, mrate): Transition Markov Matrix for the desired, possibly non-uniform, distribution vector ddv and respective mixing rate
@@ -134,15 +135,12 @@ def main():
     desired_distribution_ = np.asarray([0.1, 0.3, 0.4, 0.2])
     adj_matrix_optimized, eigenvalue = adjency_matrix_sdp_optimization(Aunopt)
     markov_matrix = __metropolis_hastings(adj_matrix_optimized, desired_distribution_)
-    print("Using Semidefinite Programming techniques...")
-    print(f"The optimal eigenvalue is: {eigenvalue}")
-    print(f"Aopt solution is: \n{adj_matrix_optimized}")
-    print(f"Resulting Markov Matrix is: \n{markov_matrix}")
+    print(f"\nUsing Semidefinite Programming techniques... The optimal eigenvalue is: {eigenvalue}\n")
+    print(f"Aopt solution is: \n{adj_matrix_optimized}\n\nResulting Markov Matrix is: \n{markov_matrix}")
     print("\n########\n########\n########\n########\n")
-    print("Using Global Optimization techniques...")
     markov_matrix, eigenvalue = optimal_bilevel_mh_transition_matrix(Aunopt, desired_distribution_)
-    print(f"The optimal eigenvalue is: {eigenvalue}")
-    print(f"Resulting Markov Matrix is: \n{markov_matrix.transpose()}")
+    print(f"Using Global Optimization techniques... The matrix normal is: {eigenvalue}")
+    print(f"\nResulting Markov Matrix is: \n{markov_matrix.transpose()}")
 
 
 if __name__ == "__main__":
