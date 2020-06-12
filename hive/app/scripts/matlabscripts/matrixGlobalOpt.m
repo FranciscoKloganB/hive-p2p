95c17c89bd0182a07f9a3405f1466ea78996508c
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-06-12 13:51:34 +0100

    Saving matlab script

---
+++ hive/app/scripts/matlabscripts/matrixGlobalOpt.m
@@ -0,0 +1,17 @@
+function [ToptValue, ToptValueMR] = matrixGlobalOpt(A, v_, n)
+    clearvars;
+    close all;
+
+    Topt = sdpvar(n,n,'full');
+
+    F = [Topt(:) >= 0, ...
+        Topt * ones(n,1) == ones(n,1), ...
+        Topt.*(ones(n)-A) == 0, ...
+        v_*Topt == v_];
+    
+    S = sdpsettings('solver', 'BMIBNB');
+    optimize(F, norm(Topt-1/n*ones(n),2), S);
+    
+    ToptValue = value(Topt);
+    ToptValueMR = max(abs(eig(Msdp-1/n*ones(n))));
+end
