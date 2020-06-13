f774df12659efeff80f8f11d7ea85701c663a7f8
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-06-13 11:03:38 +0100

    Update matrixGlobalOpt.m

--- hive/app/scripts/matlabscripts/matrixGlobalOpt.m
+++ hive/app/scripts/matlabscripts/matrixGlobalOpt.m
@@ -1,21 +1,15 @@
 function [ToptValue, ToptValueMR] = matrixGlobalOpt(A, v_)
     n = length(v_);
-    U = ones(n) / n;
-
-    display(A)
-    display(v_)
-
-    Topt = sdpvar(n, n, 'full');
-    F = [Topt(:) >= 0, ...
-        Topt * ones(n, 1) == ones(n, 1), ...
-        Topt .* (ones(n) - A) == 0, ...
-        v_ * Topt == v_];
+    U = ones(n)/n;
+    
+    Topt = sdpvar(n,n,'full');
+    F = [Topt(:) >= 0,...
+        Topt*ones(n,1) == ones(n, 1),...
+        Topt.*(ones(n)-A) == 0,...
+        v_*Topt==v_];
     S = sdpsettings('solver', 'BMIBNB');
-    optimize(F, norm(Topt - U, 2), S);
+    optimize(F,norm(Topt-U,2),S);
 
     ToptValue = value(Topt);
-    display(ToptValue)
-
     ToptValueMR = max(abs(eig(Topt - U)));
-    display(ToptValueMR)
 end
