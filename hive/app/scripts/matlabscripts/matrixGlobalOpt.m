8c4500f0db6fd8af2a247bd90188fd5d59ded6ff
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-06-15 10:45:15 +0100

    Update matrixGlobalOpt.m

--- hive/app/scripts/matlabscripts/matrixGlobalOpt.m
+++ hive/app/scripts/matlabscripts/matrixGlobalOpt.m
@@ -1,15 +1,32 @@
 function [ToptValue, ToptValueMR] = matrixGlobalOpt(A, v_)
     n = length(v_);
-    U = ones(n)/n;
+    U = ones(n) / n;
     
-    Topt = sdpvar(n,n,'full');
+    Topt = sdpvar(n, n, 'full');
     F = [Topt(:) >= 0,...
-        Topt*ones(n,1) == ones(n, 1),...
-        Topt.*(ones(n)-A) == 0,...
-        v_*Topt==v_];
+        Topt * ones(n,1) == ones(n, 1),...
+        Topt .* (ones(n)-A) == 0,...
+        v_ * Topt==v_];
     S = sdpsettings('solver', 'bmibnb');
-    optimize(F,norm(Topt-U,2),S);
 
-    ToptValue = value(Topt);
-    ToptValueMR = max(abs(eig(Topt - U)));
+    diagnostics = optimize(F, norm(Topt-U, 2), S);
+
+    if diagnostics.problem == 0
+        sprintf('Feasible')
+        ToptValue = value(Topt);
+        ToptValueMR = max(abs(eig(Topt - U)));
+        display(class(ToptValue))
+        display(class(ToptValueMR))
+        return
+    elseif diagnostics.problem == 1
+        sprintf('Unfeasible')
+    else
+        sprintf('Something else happened (ec: %d)', diagnostics.problem)
+    end
+
+    ToptValue = [];
+    ToptValueMR = 1.0;
+    display(class(ToptValue))
+    display(class(ToptValueMR))
+    return
 end
