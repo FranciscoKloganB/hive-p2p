dd13bed67953097f54f4480d2e4a9a26b6b7792d
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-06-15 11:23:24 +0100

    Update convex_optimization_mh.py

--- hive/app/utils/convex_optimization_mh.py
+++ hive/app/utils/convex_optimization_mh.py
@@ -163,7 +163,6 @@ def third_method_as_matlab(A: np.ndarray, v_: np.ndarray):
         matlab_A = matlab.double(A.tolist())
         matlab_v_ = matlab.double(v_.tolist())
         Topt, mr = eng.matrixGlobalOpt(matlab_A, matlab_v_, nargout=2)
-        # print(f"Type: {type(mr)}, Mixing Rate: {mr}:\nType: {type(Topt)}, Topt:\n{Topt}")
     except matlab.engine.EngineError as error:
         print(str(error))
 
