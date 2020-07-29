58b51145f98a8ab899b218ee0056716792846359
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-07-29 12:41:08 +0100

    Docstringed exceptions that remain in use

--- hive/app/domain/exceptions/DistributionShapeError.py
+++ hive/app/domain/exceptions/DistributionShapeError.py
@@ -1,7 +1,5 @@
 class DistributionShapeError(Exception):
-    """
-    Raised when a vector shape is expected to match a matrix's column or row shape and doesn't
-    """
+    """Raised when a vector shape is expected to match a matrix's column or row shape and doesn't."""
     def __init__(self, value=""):
         self.value = value
 
--- hive/app/domain/exceptions/MatrixNotSquareError.py
+++ hive/app/domain/exceptions/MatrixNotSquareError.py
@@ -1,7 +1,5 @@
 class MatrixNotSquareError(Exception):
-    """
-    Raised when a function expects a square (N * N) matrix in any representation and some other dimensions are given
-    """
+    """Raised when a function expects a square (N * N) matrix in any representation and some other dimensions are given"""
     def __init__(self, value=""):
         self.value = value
 
