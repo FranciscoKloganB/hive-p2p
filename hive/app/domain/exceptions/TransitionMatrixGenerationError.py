938dbb2b850698845a114c9d30a9e122f5fae104
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-07-29 12:39:33 +0100

    Deleted two unused exceptions

--- hive/app/domain/exceptions/TransitionMatrixGenerationError.py
+++
@@ -1,9 +0,0 @@
-class TransitionMatrixGenerationError(Exception):
-    """
-    Raised when a function fails to generate a transition matrix with given inputs. Likely when optimization is not possible.
-    """
-    def __init__(self, value=""):
-        self.value = value
-
-    def __str__(self):
-        return self.value
