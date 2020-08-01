79d7ecb588ce06abcd88b9d1325ce36d83b26278
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-08-01 12:45:53 +0100

    Update hive_simulation.py

--- hive/app/hive_simulation.py
+++ hive/app/hive_simulation.py
@@ -32,25 +32,9 @@ err_message = ("Invalid arguments. You must specify -f or -d running options\n",
                "$ python hive_simulation.py -d")
 
 
-def multithreaded_main(simfile: str, iters: int) -> None:
-    for run in range(iters):
-        hm.Hivemind(simfile_name=simfile, sid=run).execute_simulation()
-
-
-def execute_simulation(sname: str, sid: int) -> None:
-    """Executes one instance of the simulation
-
-    Args:
-        sname:
-            The name of the simulation file to be executed.
-        sid:
-            An integer that uniquely identifies the simulation execution.
-    """
-    hm.Hivemind(simfile_name=sname, sid=sid).execute_simulation()
-
-
-def __makedirs__() -> None:
-    """Creates required simulation working directories"""
+# region Module Private Functions
+def __makedirs() -> None:
+    """Creates required simulation working directories if they do not exist."""
     if not os.path.exists(SHARED_ROOT):
         os.makedirs(SHARED_ROOT)
 
@@ -61,61 +45,104 @@ def __makedirs__() -> None:
         os.makedirs(OUTFILE_ROOT)
 
 
-def __can_exec_simfile__(sname: str) -> None:
-    """Verifies if input simulation file name exists in working simdirectory."""
-    if sname == "":
-        sys.exit("Simulation file name can not be blank.")
-
+def __can_exec_simfile(sname: str) -> None:
+    """Verifies if input simulation file name exists in ~/*/SIMULATION_ROOT"""
     spath = os.path.join(SIMULATION_ROOT, sname)
     if not os.path.exists(spath):
         sys.exit("Specified simulation file does not exist in SIMULATION_ROOT.")
 
 
-def main(sdir: bool, multithread: bool, sname: str, iters: int) -> None:
-    __makedirs__()
-    __can_exec_simfile__(sname)
+def __execute_simulation(sname: str, sid: int) -> None:
+    """Executes one instance of the simulation
 
-    if not multithread:
+    Args:
+        sname:
+            The name of the simulation file to be executed.
+        sid:
+            A sequence number that identifies the simulation execution instance.
+    """
+    hm.Hivemind(sname, sid).execute_simulation()
+# endregion
+
+
+def multi_threaded_main(sdir: bool, sname: str, iters: int) -> None:
+    if sdir:
+        snames = os.listdir(SIMULATION_ROOT)
+        for sn in snames:
+            for i in range(iters):
+                __execute_simulation(sn, i)
+    else:
+        __can_exec_simfile(sname)
         for i in range(iters):
-            execute_simulation(sname, i)
-        # input_simulation_files: List[str] = os.listdir(SIMULATION_ROOT)
-        # for name in input_simulation_files:
-        #     for i in range(iters):
-        #         execute_simulation(name, i)
-        return
+            __execute_simulation(sname, i)
 
-    multithreaded_main(sname, iters)
-    return
+
+def single_threaded_main(sdir, sname, iters):
+    if sdir:
+        snames = os.listdir(SIMULATION_ROOT)
+        for sn in snames:
+            for i in range(iters):
+                __execute_simulation(sn, i)
+    else:
+        __can_exec_simfile(sname)
+        for i in range(iters):
+            __execute_simulation(sname, i)
+
+
+def main(multithread: bool, sdir: bool, sname: str, iters: int) -> None:
+    """Receives user input and initializes the simulation process.
+
+    Args:
+        multithread:
+            Indicates if multiple simulation instances should run in parallel.
+        sdir:
+            Indicates if the user wishes to execute all simulation files
+            that exist in :py:const:`~globals.globals.SIMULATION_ROOT` or
+            if he wishes to run one single simulation file, which must be
+            explicitly specified in `sname`.
+        sname:
+            When `sdir` is set to False, `sname` needs to be specified as a
+            non blank string containing the name of the simulation file to
+            be executed. The named file must exist in
+            :py:const:`~globals.globals.SIMULATION_ROOT`.
+        iters:
+            The number of times the same simulation file should be executed.
+    """
+    if multithread:
+        multi_threaded_main(sdir, sname, iters)
+    else:
+        single_threaded_main(sdir, sname, iters)
 
 
 if __name__ == "__main__":
+    __makedirs()
+
     threading = False
     simdirectory = False
     simfile = None
     iterations = 30
 
     try:
-        options, args = getopt.getopt(sys.argv[1:], "tdf:r:", ["threading",
-                                                               "directory",
-                                                               "file=",
-                                                               "iters="])
+        short_opts = "tdf:i:"
+        long_opts = ["threading", "directory", "file=", "iters="]
+        options, args = getopt.getopt(sys.argv[1:], short_opts, long_opts)
 
         for options, args in options:
             if options in ("-t", "--threading"):
                 threading = True
-
             if options in ("-d", "--directory"):
                 simdirectory = True
             elif options in ("-f", "--file"):
                 simfile = str(args).strip()
-
+                if simfile == "":
+                    sys.exit("Simulation file name can not be blank.")
             if options in ("-i", "--iters"):
                 iterations = int(str(args).strip())
 
         if simfile or simdirectory:
-            main(simdirectory, threading, simfile, iterations)
+            main(threading, simdirectory, simfile, iterations)
         else:
-            print(err_message)
+            sys.exit(err_message)
 
     except getopt.GetoptError:
-        print(err_message)
+        sys.exit(err_message)
