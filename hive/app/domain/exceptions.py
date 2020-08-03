211afde03c9eed2bc77f74c818a1ed108cc5a53c
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-08-03 13:18:19 +0100

    Update exceptions.py

--- hive/app/domain/exceptions.py
+++ hive/app/domain/exceptions.py
@@ -1,9 +1,10 @@
-"""This module includes domain specific classes that inherit from Python's
-builtin Exception class."""
+"""Module with classes that inherit from Python's builtin Exception class
+and represent domain specific errors."""
 
 
 class DistributionShapeError(Exception):
-    """Raised when a vector shape is expected to match a matrix's column or row shape and doesn't."""
+    """Raised when a vector shape is expected to match a matrix's column or
+    row shape and does not."""
     def __init__(self, value=""):
         self.value = value
 
@@ -12,7 +13,8 @@ class DistributionShapeError(Exception):
 
 
 class MatrixNotSquareError(Exception):
-    """Raised when a function expects a square (N * N) matrix in any representation and some other dimensions are given"""
+    """Raised when a function expects a square (N * N) matrix in any
+    representation and some other dimensions are given."""
     def __init__(self, value=""):
         self.value = value
 
