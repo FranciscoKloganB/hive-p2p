3da786f85a1a15deb7b78b4995695999a2fa01c4
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-08-10 11:53:02 +0100

    Update convertions.py

--- hive/app/utils/convertions.py
+++ hive/app/utils/convertions.py
@@ -136,7 +136,11 @@ def class_name_to_obj(module_name: str, class_name: str, args: List[Any]) -> Any
     Examples:
         You could call this function like so::
 
-            str_to_class("domain.master_servers", "Hivemind", ["f.jpg", 1, 80]).
+            h = class_name_to_obj(MASTER_SERVERS, "Hivemind", ["f.jpg", 1, 80])
+
+        This would be equivalent to calling::
+
+            h = Hivemind("f.jpg", 1, 80)
     """
     try:
         module_ = importlib.import_module(module_name)
