5b852aa2658ec39842df733790759adace61a500
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-08-14 13:50:15 +0100

    Update hive_simulation.py

--- hive/app/hive_simulation.py
+++ hive/app/hive_simulation.py
@@ -164,7 +164,7 @@ if __name__ == "__main__":
     duration = 720
 
     master_class = "Hivemind"
-    cluster_class = "Cluster"
+    cluster_class = "HiveCluster"
     node_class = "HiveNode"
 
     try:
