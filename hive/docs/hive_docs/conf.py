884b8bd28f62df3199ded031c12fb414d1a9ed14
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-08-19 11:53:13 +0100

    Added extensions and moved things down

--- hive/docs/hive_docs/conf.py
+++ hive/docs/hive_docs/conf.py
@@ -31,6 +31,9 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',
     'sphinx.ext.napoleon'
 ]
 
