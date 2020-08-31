a0c450d2cf8b77aa8c9458329be5bc3e8f8f473b
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2020-08-31 14:48:14 +0100

    better enums docstring

--- hive/app/domain/helpers/enums.py
+++ hive/app/domain/helpers/enums.py
@@ -5,12 +5,13 @@ from enum import Enum
 
 
 class Status(Enum):
-    """Enumerator that defines connectivity status of a network node
+    """Enumerator that defines connectivity status of a network node.
 
     The following status exist:
-        * SUSPECT: Network node may be offline;
-        * OFFLINE: Network node is offline;
-        * ONLINE: Network node is online;
+
+    * SUSPECT: Network node may be offline;
+    * OFFLINE: Network node is offline;
+    * ONLINE: Network node is online;
     """
     SUSPECT: int = 1
     OFFLINE: int = 2
@@ -18,16 +19,17 @@ class Status(Enum):
 
 
 class HttpCodes(Enum):
-    """Enumerator class used to represent HTTP response codes
+    """Enumerator class used to represent HTTP response codes.
 
     The following codes are considered:
-        * DUMMY: Dummy value. Use when no valid HTTP code exists;
-        * OK: Callee accepts the sent file;
-        * BAD_REQUEST: Callee refuses the integrity of sent file;
-        * NOT_FOUND: Callee is not a member of the network;
-        * NOT_ACCEPTABLE: Callee already has a file with same Id;
-        * TIME_OUT: Message lost in translation;
-        * SERVER_DOWN: Metadata server is offline;
+
+    * DUMMY: Dummy value. Use when no valid HTTP code exists;
+    * OK: Callee accepts the sent file;
+    * BAD_REQUEST: Callee refuses the integrity of sent file;
+    * NOT_FOUND: Callee is not a member of the network;
+    * NOT_ACCEPTABLE: Callee already has a file with same Id;
+    * TIME_OUT: Message lost in translation;
+    * SERVER_DOWN: Metadata server is offline;
     """
     DUMMY: int = -1
     OK: int = 200
