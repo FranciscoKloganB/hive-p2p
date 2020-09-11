ae8308e623d8f4c9fb5866f873e6b2d2529012a9
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2020-09-11 14:04:24 +0100

    Update normal_distribution_generator.py

--- hive/app/scripts/python/normal_distribution_generator.py
+++ hive/app/scripts/python/normal_distribution_generator.py
@@ -39,48 +39,6 @@ def generate_samples(surveys: int = 10,
     return results
 
 
-def generate_samples_extended(bin_count: int = 7001,
-                              sample_count: int = 7001
-                              ) -> Tuple[np.array, np.array]:
-    """Generates samples from a normal distribution.
-
-    Notes:
-        A `bin_count` of 7001 represents all values between [30.00, 100.00]
-        with 0.01 step, whereas a `bin_count` of 800001 would represents all
-        values between [20.0000, 100.000] with 0.0001 step. To let
-        matplotlib.skewnorm module define an automatic number of bins use
-        bin_count='auto'.
-
-    Args:
-        bin_count:
-             The number of bins to be created.
-        sample_count:
-             The number of skewed results to be drawn.
-
-    Returns:
-        The sampled bins and respective frequencies.
-    """
-    results: np.array = generate_samples(sample_count)
-    bin_density, bins, patches = plt.hist(results, bins=bin_count, density=True)
-
-    size: int = len(bin_density)
-
-    total_density: float = 0.0
-    for i in range(size):
-        total_density += bin_density[i]
-
-    total_probability: float = 0.0
-    bin_probability = bin_density
-    for i in range(size):
-        bin_probability[i] = bin_density[i] / total_density
-        total_probability += bin_probability[i]
-
-    if total_probability != 1.0:
-        logging.warning("probability_compensation: " + str(1.0 - total_probability))
-
-    return results, bin_probability
-
-
 def plot_uptime_distribution(bin_count: Union[int, str] = 'auto',
                              mean: float = 34.0,
                              std: float = 30.0) -> None:
