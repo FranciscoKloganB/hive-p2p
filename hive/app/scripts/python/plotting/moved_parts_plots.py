c9cd888141e54787ad3604db7572dbb21cdb19bc
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2020-09-24 21:04:42 +0100

    Update moved_parts_plots.py

--- hive/app/scripts/python/plotting/moved_parts_plots.py
+++ hive/app/scripts/python/plotting/moved_parts_plots.py
@@ -1,6 +1,3 @@
-"""
-This script collects data
-"""
 import getopt
 import json
 import os
@@ -8,60 +5,59 @@ import sys
 from itertools import zip_longest
 from typing import List, Dict
 
-import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.font_manager import FontProperties
+
+import matplotlib.pyplot as plt
+import _matplotlib_configs as cfg
 
 
-def usage():
-    print(" -------------------------------------------------------------------------")
-    print(" Francisco Barros (francisco.teixeira.de.barros@tecnico.ulisboa.pt\n")
-    print(" Collect data regarding moved parts from a collection of output files located in referenced directory\n")
-    print(" Typical usage: python moved_parts_plots.py --meandir=mean32 --istate=a\n")
-    print(" Display all optional flags and other important notices: hive_simulation.py --help\n")
-    print(" -------------------------------------------------------------------------\n")
-    sys.exit(" ")
-
-
-def plotvalues(epoch_means, mean, terminations, directory, state):
+def plot_values(terminations):
     plt.figure()
-    # plt.title("Moved Parts Analysis - {}i{}".format(directory, state))
+
     plt.xlabel("Epoch")
     plt.ylabel("Avg. Moved Parts")
-    plt.xlim(0, 720)
+
+    plt.xlim(0, epochs)
     plt.ylim(0, 1100)
+
     # Trace global mean
-    plt.axhline(y=mean,  label="global average", color='c', linestyle='-')
+    plt.axhline(y=instances_mean, label="global mean", color='c', linestyle='-')
+
     # Trace cumulative means
-    plt.plot(epoch_means, label="cumulative average")
+    plt.plot(cs_avg_list, label="global cumulative means")
+
     # Trace terminations
     # plt.axvline(x=terminations.pop(), label="at least one simulation instance ended", color='y', linestyle='--')
     # for epoch in terminations:
     #     plt.axvline(x=epoch, color='y', linestyle='--')
+
     # Display legends
     plt.legend(loc='lower right')
-    # plt.show()
-    plt.savefig("{}-{}-{}".format("blocks_moved", directory, state), prop=FontProperties().set_size('small'))
+
+    plt.show()
 
 
-def process_file(filepath, avg_moved_parts, terminated_at_acount):
-    with open(filepath) as instance:
-        # Serialize json file
-        json_obj = json.load(instance)
-        # Update terminated_at_count so that epochs' means is not skewed by the'filled' zeros in zip_longest
-        terminated = json_obj["terminated"]
-        if terminated in terminated_at_acount:
-            terminated_at_acount[terminated] += 1
+def process_file(outfile_json):
+    with open(outfile_json) as instance:
+        j = json.load(instance)
+        # Update terminated_at_count so that cumsum mean isn't skewed by 'fill'.
+        # Important when different instances of the same simulation terminate
+        # at different epoch times.
+        terminated = j["terminated"]
+        if terminated in terminated_at_count:
+            terminated_at_count[terminated] += 1
         else:
-            terminated_at_acount[terminated] = 1
-        # Epoch data from [0, terminated) w.r.t. number of blocks_moved of the current simulation instance
-        data = json_obj["blocks_moved"][:terminated]
-        # Calculate and store the mean of current simulation instance
-        avg_moved_parts.append(np.mean(data))
-        # Calculate and store the mean at each epoch i of the current simulation instance
+            terminated_at_count[terminated] = 1
+        # Get the simulation instance relevant data from [0, terminated).
+        # Terminated should be smaller or equal than __main__.epochs variable.
+        data = j["blocks_moved"][:terminated]
+        # Calculate and store the flat mean of the instance.
+        sim_averages.append(np.mean(data))
+        # Calculate and store the mean at each epoch i of the instance.
         temp_list = []
         for i in range(terminated):
-            # Calculate the until current epoch; Since i starts at 0, we divide by i + 1
+            # Calculate the until current epoch
+            # Since i starts at 0, we divide by i + 1
             temp_list.append(sum(data[:i]) / (i + 1))
         # Now return temp_list so it can be zipped by caller
         return temp_list
@@ -87,50 +83,59 @@ def get_epochs_means(avg_moved_parts_epoch, terminated_at_acount):
             at += 1
 
 
-def main(directory, state):
-    path = os.path.join(os.path.abspath(os.path.join(os.getcwd(), '..', '..', 'static', 'outfiles')), directory, state)
-    avg_moved_parts: List[float] = []
-    avg_moved_parts_epoch: List[float] = [0.0] * 720
-    terminated_at_acount: Dict[int, int] = {}
-    for filename in os.listdir(path):
-        _ = process_file(os.path.join(path, filename), avg_moved_parts, terminated_at_acount)
-        avg_moved_parts_epoch = [sum(n) for n in zip_longest(avg_moved_parts_epoch, _, fillvalue=0)]
-    # Calculate the global mean
-    avg_moved_parts_mean = np.mean(avg_moved_parts)
-    # Calculate the global mean at epoch i; Since we have a sum of means, at each epoch, we only need to divide each element by the number of seen instances
-    avg_moved_parts_epoch = get_epochs_means(avg_moved_parts_epoch, terminated_at_acount)
-
-    plotvalues(avg_moved_parts_epoch, avg_moved_parts_mean, [*terminated_at_acount], directory, state)
-
-
 if __name__ == "__main__":
-    meandir = None
-    istate = None
+    epochs = 0
+    patterns = []
+
+    short_opts = "e:p:"
+    long_opts = ["epochs=", "patterns="]
     try:
-        options, args = getopt.getopt(sys.argv[1:], "ud:i:", ["usage", "meandir=", "istate="])
+        options, args = getopt.getopt(sys.argv[1:], short_opts, long_opts)
+
         for options, args in options:
-            if options in ("-u", "--usage"):
-                usage()
-                sys.exit(0)
-            elif options in ("-d", "--meandir"):
-                meandir = str(args).strip()
-            elif options in ("-i", "--istate"):
-                istate = str(args).strip()
+            if options in ("-e", "--epochs"):
+                epochs = int(str(args).strip())
+            if options in ("-p", "--patterns"):
+                patterns = str(args).strip()
+                if not patterns:
+                    sys.exit(f"Blank pattern is not a valid pattern.")
+                patterns = patterns.split(",")
+
+        if epochs <= 0:
+            sys.exit(f"Must specify epochs to allocate the plot's data arrays.")
+
     except getopt.GetoptError:
-        usage()
-        sys.exit(0)
-    if (meandir and istate):
-        main(meandir, istate)
-    else:
-        main('mean32', 'a')
-        main('mean32', 'i')
-        main('mean32', 'u')
-        main('mean56', 'a')
-        main('mean56', 'i')
-        main('mean56', 'u')
-        main('mean78', 'a')
-        main('mean78', 'i')
-        main('mean78', 'u')
-        main('mean90', 'a')
-        main('mean90', 'i')
-        main('mean90', 'u')
+        sys.exit("Usage: python outfile_plotter.py -f outfile.json")
+    except ValueError:
+        sys.exit("Execution arguments should have the following data types:\n"
+                 "  --epochs -e (int)\n"
+                 "  --patterns -p (comma seperated list of str)\n")
+
+    directory = os.path.join(
+        os.path.abspath(
+            os.path.join(os.getcwd(), '..', '..', 'static', 'outfiles')))
+
+    outfiles_view = os.listdir(directory)
+    for pattern in patterns:
+        outfiles_view = filter(lambda file: pattern in file, outfiles_view)
+
+    # w.r.t. to named json field...
+    # Stores flat simulations' mean values, e.g., 30 iterations' mean.
+    sim_averages: List[float] = []
+    # Stores a simulation's cumulative mean on an epoch basis.
+    cs_avg_list: List[float] = [0.0] * epochs
+    # Stores how many instances terminate at a given epoch.
+    terminated_at_count: Dict[int, int] = {}
+
+    for file in outfiles_view:
+        filepath = os.path.join(directory, file)
+        _ = process_file(filepath)
+        cs_avg_list = [sum(n) for n in zip_longest(cs_avg_list, _, fillvalue=0)]
+
+    # Calculate the instances' global flat mean
+    instances_mean = np.mean(sim_averages)
+    # Calculate the instances' global cumulative mean on a epoch by epoch basis.
+    # Since we have a sum of means, at each epoch, we divide each element by
+    # the number of visited instances, on an interval by interval basis.
+    cs_avg_list = get_epochs_means(cs_avg_list, terminated_at_count)
+    plot_values(list(terminated_at_count))
