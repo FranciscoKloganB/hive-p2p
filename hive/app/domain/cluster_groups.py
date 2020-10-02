3cad7c953067ff96fceb2a33a34a5f624bc29fba
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2020-10-02 21:39:54 +0100

    Update cluster_groups.py

--- hive/app/domain/cluster_groups.py
+++ hive/app/domain/cluster_groups.py
@@ -987,8 +987,8 @@ class HiveClusterPerfect(HiveCluster):
                  sim_id: int = 0,
                  origin: str = "") -> None:
         super().__init__(master, file_name, members, sim_id, origin)
+        es.set_loss_chance(0.0) 
         self.corruption_chances: List[float] = [0.0, 1.0]
-        es.set_loss_chance(0.0)  # environment_settings.set_loss_chance
 
     # region Swarm guidance structure management
     def new_desired_distribution(
