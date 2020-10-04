8918fdcd10d2046f187f8114efbf07ff781e0930
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2020-10-04 11:20:55 +0100

    Revert "Revert "use atol =  clip(1 / Nodes, 0, ATOL) instead of fixed 5% atol because of big networks always converging.""
    This reverts commit 0f7926c48dfb7b17688306dc4545ca2dd9be216d.

--- hive/app/domain/cluster_groups.py
+++ hive/app/domain/cluster_groups.py
@@ -919,7 +919,7 @@ class HiveCluster(Cluster):
         """
         ptotal = self.file.existing_replicas
         target = self.v_.multiply(ptotal)
-        atol = np.clip(es.ATOL, 0.0, 1.0) * ptotal
+        atol = np.clip(1 / self.original_size, 0.0, es.ATOL) * ptotal
         result = np.allclose(self.cv_, target, rtol=es.RTOL, atol=atol)
         if es.DEBUG:
             print(f"converged: {result}\n"
@@ -940,7 +940,8 @@ class HiveCluster(Cluster):
         distance = np.abs(self.v_.subtract(self.avg_))
         magnitude = np.sqrt(distance).sum(axis=0).item()
 
-        goaled = np.allclose(self.avg_, self.v_, rtol=es.RTOL, atol=es.ATOL)
+        # atol = np.clip(1 / self.original_size, 0.0, es.ATOL)
+        goaled = np.allclose(self.avg_, self.v_, rtol=es.RTOL, atol=0.0)
 
         self.file.logger.log_topology_goal_performance(goaled, magnitude)
 
