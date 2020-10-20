4d9fa9367d85239d339fca14b6cc13cdaaf18e63
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2020-10-20 12:24:58 +0200

    Update convergence_analysis_v2_parts.py

--- hive/app/scripts/python/plotting/convergence_analysis_v2_parts.py
+++ hive/app/scripts/python/plotting/convergence_analysis_v2_parts.py
@@ -90,10 +90,10 @@ def __create_double_boxplot__(
         plt.savefig(f"{plots_directory}/{figname}-{ns}_O{opt}_Pde{pde}_Pml{pml}.{figext}",
                     format=figext, bbox_inches="tight")
     return fig, ax
-
 # endregion
 
 
+# region Boxplots
 def boxplot_bandwidth(rl: int = 3, figname: str = "BW") -> None:
     filesize = 47185920  # bytes
     data_dict = {k: [] for k in source_keys}
@@ -116,55 +116,6 @@ def boxplot_bandwidth(rl: int = 3, figname: str = "BW") -> None:
         figname=figname, figext=image_ext)
 
 
-def barchart_instantaneous_convergence_vs_progress(
-        bucket_size: int = 5, figname: str = "ICC") -> None:
-    # region create buckets of 5%
-    bucket_count = int(100 / bucket_size)
-    epoch_buckets = [i * bucket_size for i in range(1, bucket_count + 1)]
-    # endregion
-
-    data_dict = {k: [] for k in source_keys}
-    for src_key, outfiles_view in sources_files.items():
-        epoch_cc = {i: 0 for i in range(1, epochs + 1)}
-        for filename in outfiles_view:
-            filepath = os.path.join(directory, filename)
-            with open(filepath) as outfile:
-                outdata = json.load(outfile)
-                sets = outdata["convergence_sets"]
-                for s in sets:
-                    for e in s:
-                        epoch_cc[e] += 1
-        # region create buckets of 5% and allocate the buuckets' values
-        epoch_vals = [0] * bucket_count
-        for i in range(bucket_count):
-            low = bucket_size * i
-            high = bucket_size * (i + 1)
-            for epoch, count in epoch_cc.items():
-                if low < epoch <= high:
-                    epoch_vals[i] += count
-        data_dict[src_key] = epoch_vals
-        # endregion
-
-    bar_locations = np.asarray(epoch_buckets)
-    bar_width = bucket_size * 0.25
-
-    fig, ax = plt.subplots()
-    plt.suptitle("convergence observations as simulations' progress", fontproperties=cfg.fp_title)
-    plt.xlabel("simulations' progress (%)", labelpad=cfg.labels_pad, fontproperties=cfg.fp_axis_labels)
-    plt.ylabel(r"c$_{t}$ count", labelpad=cfg.labels_pad, fontproperties=cfg.fp_axis_labels)
-    plt.xlim(bucket_size - bucket_size * 0.75, 100 + bucket_size*0.8)
-    plt.ylim(0, 100)
-    plt.xticks(rotation=75, fontsize="x-large", fontweight="semibold")
-    plt.yticks(fontsize="x-large", fontweight="semibold")
-    ax.set_xticks(epoch_buckets)
-    for i in range(len(source_keys)):
-        key = source_keys[i]
-        epoch_vals = data_dict[key]
-        ax.bar(bar_locations + (bar_width * i) - 0.5 * bar_width, epoch_vals, width=bar_width)
-    ax.legend([f"{x} parts" for x in source_keys], prop=cfg.fp_axis_legend)
-    __save_figure__(figname, image_ext)
-
-
 def boxplot_first_convergence(figname: str = "FIC"):
     data_dict = {k: [] for k in source_keys}
     for src_key, outfiles_view in sources_files.items():
@@ -181,39 +132,6 @@ def boxplot_first_convergence(figname: str = "FIC"):
         suptitle="clusters' first instantaneous convergence",
         xlabel="config", ylabel="epoch",
         figname=figname, figext=image_ext)
-
-
-def piechart_avg_convergence_achieved(figname: str = "GA") -> None:
-    data_dict = {k: [] for k in source_keys}
-    for src_key, outfiles_view in sources_files.items():
-        data = [0.0, 0.0]
-        for filename in outfiles_view:
-            filepath = os.path.join(directory, filename)
-            with open(filepath) as outfile:
-                outdata = json.load(outfile)
-                classifications = outdata["topologies_goal_achieved"]
-                for success in classifications:
-                    data[0 if success else 1] += 1
-        data_dict[src_key] = list(data)
-
-    s = len(source_keys)
-    wedge_labels = ["achieved eq.", "has not achieved eq."]
-    fig, axes = plt.subplots(1, s, figsize=(s*3, s))
-    plt.suptitle("clusters (%) achieving the selected equilibrium on average", fontproperties=cfg.fp_title, x=0.51)
-    for i, ax in enumerate(axes.flatten()):
-        ax.axis('equal')
-        src_key = source_keys[i]
-        wedges, _, _ = ax.pie(
-            data_dict[src_key],
-            startangle=90, autopct='%1.1f%%',
-            labels=wedge_labels, labeldistance=None,
-            textprops={'color': 'white', 'weight': 'bold'}
-        )
-        ax.set_xlabel(f"{src_key} parts", fontproperties=cfg.fp_axis_legend)
-    plt.legend(labels=wedge_labels, ncol=s, frameon=False,
-               loc="best", bbox_to_anchor=(0.5, -0.2),
-               prop=cfg.fp_axis_legend)
-    __save_figure__(figname, image_ext)
 
 
 def boxplot_percent_time_instantaneous_convergence(figname: str = "TSIC"):
@@ -270,6 +188,89 @@ def boxplot_avg_convergence_magnitude_distance(figname: str = "MD"):
 
     plt.xticks(range(0, len(source_keys) * 2, 2), source_keys, rotation=45, fontsize="x-large", fontweight="semibold")
     plt.xlim(-2, len(source_keys) * 2)
+    __save_figure__(figname, image_ext)
+# endregion
+
+
+def barchart_instantaneous_convergence_vs_progress(
+        bucket_size: int = 5, figname: str = "ICC") -> None:
+    # region create buckets of 5%
+    bucket_count = int(100 / bucket_size)
+    epoch_buckets = [i * bucket_size for i in range(1, bucket_count + 1)]
+    # endregion
+
+    data_dict = {k: [] for k in source_keys}
+    for src_key, outfiles_view in sources_files.items():
+        epoch_cc = {i: 0 for i in range(1, epochs + 1)}
+        for filename in outfiles_view:
+            filepath = os.path.join(directory, filename)
+            with open(filepath) as outfile:
+                outdata = json.load(outfile)
+                sets = outdata["convergence_sets"]
+                for s in sets:
+                    for e in s:
+                        epoch_cc[e] += 1
+        # region create buckets of 5% and allocate the buuckets' values
+        epoch_vals = [0] * bucket_count
+        for i in range(bucket_count):
+            low = bucket_size * i
+            high = bucket_size * (i + 1)
+            for epoch, count in epoch_cc.items():
+                if low < epoch <= high:
+                    epoch_vals[i] += count
+        data_dict[src_key] = epoch_vals
+        # endregion
+
+    bar_locations = np.asarray(epoch_buckets)
+    bar_width = bucket_size * 0.25
+
+    fig, ax = plt.subplots()
+    plt.suptitle("convergence observations as simulations' progress", fontproperties=cfg.fp_title)
+    plt.xlabel("simulations' progress (%)", labelpad=cfg.labels_pad, fontproperties=cfg.fp_axis_labels)
+    plt.ylabel(r"c$_{t}$ count", labelpad=cfg.labels_pad, fontproperties=cfg.fp_axis_labels)
+    plt.xlim(bucket_size - bucket_size * 0.75, 100 + bucket_size*0.8)
+    plt.ylim(0, 100)
+    plt.xticks(rotation=75, fontsize="x-large", fontweight="semibold")
+    plt.yticks(fontsize="x-large", fontweight="semibold")
+    ax.set_xticks(epoch_buckets)
+    for i in range(len(source_keys)):
+        key = source_keys[i]
+        epoch_vals = data_dict[key]
+        ax.bar(bar_locations + (bar_width * i) - 0.5 * bar_width, epoch_vals, width=bar_width)
+    ax.legend([f"{x} parts" for x in source_keys], prop=cfg.fp_axis_legend)
+    __save_figure__(figname, image_ext)
+
+
+def piechart_avg_convergence_achieved(figname: str = "GA") -> None:
+    data_dict = {k: [] for k in source_keys}
+    for src_key, outfiles_view in sources_files.items():
+        data = [0.0, 0.0]
+        for filename in outfiles_view:
+            filepath = os.path.join(directory, filename)
+            with open(filepath) as outfile:
+                outdata = json.load(outfile)
+                classifications = outdata["topologies_goal_achieved"]
+                for success in classifications:
+                    data[0 if success else 1] += 1
+        data_dict[src_key] = list(data)
+
+    s = len(source_keys)
+    wedge_labels = ["achieved eq.", "has not achieved eq."]
+    fig, axes = plt.subplots(1, s, figsize=(s*3, s))
+    plt.suptitle("clusters (%) achieving the selected equilibrium on average", fontproperties=cfg.fp_title, x=0.51)
+    for i, ax in enumerate(axes.flatten()):
+        ax.axis('equal')
+        src_key = source_keys[i]
+        wedges, _, _ = ax.pie(
+            data_dict[src_key],
+            startangle=90, autopct='%1.1f%%',
+            labels=wedge_labels, labeldistance=None,
+            textprops={'color': 'white', 'weight': 'bold'}
+        )
+        ax.set_xlabel(f"{src_key} parts", fontproperties=cfg.fp_axis_legend)
+    plt.legend(labels=wedge_labels, ncol=s, frameon=False,
+               loc="best", bbox_to_anchor=(0.5, -0.2),
+               prop=cfg.fp_axis_legend)
     __save_figure__(figname, image_ext)
 
 
