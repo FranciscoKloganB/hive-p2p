1dc8c192b6898d93b290be0eef2b781160cf9f31
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2020-10-23 16:05:09 +0100

    Update hive_simulation.py

--- hive/app/hive_simulation.py
+++ hive/app/hive_simulation.py
@@ -16,6 +16,12 @@ working threads. For example::
 
     $ python hive_simulation.py -d --iters=1 --threading=12
 
+Warning:
+     Be aware that python's
+    :py:class:`~py:concurrent.futures.ThreadPoolExecutor` conceals/suprresses
+    any uncaught exceptions, i.e., simulations may fail to execute or log items
+    properly and no debug information will be provided
+
 If you don't have a simulation file yet, run the following instead::
 
     $ python simfile_generator.py --file=filename.json
